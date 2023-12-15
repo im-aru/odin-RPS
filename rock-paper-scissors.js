47177b31051aceae95d02c49f0c0fd59b15036c9
aru <110044109+im-aru@users.noreply.github.com>
2023-12-15 07:53:28 -0800

    Cleaned up switch statement and variable scopes
    Improved readability

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -29,50 +29,44 @@ function getPlayerChoice() {
 }
 
 //Create a function to play rock paper scissors
-function playRockPaperScissors(playerChoice, computerChoice) {
+function playRockPaperScissors() {
   //Get a new user choice
-  playerChoice = getPlayerChoice();
+  const playerChoice = getPlayerChoice();
   console.log('You have chosen ' + playerChoice);
-
   //Get a new computer choice
-  computerChoice = getComputerChoice();
+  const computerChoice = getComputerChoice();
   console.log('The computer has chosen ' + computerChoice);
 
-  //Compare answers to see who won
   switch (true) {
     //If both answers are the same, re-play the game until one side wins.
     case (playerChoice === computerChoice):
       console.log('The result is a tie! Please try again.');
       return playRockPaperScissors();
-    case (playerChoice === 'rock' && computerChoice === 'scissors'): return 'You won! Rock beats scissors.';
-    case (playerChoice === 'rock' && computerChoice === 'paper'): return 'You lost. Paper beats rock.';
-    case (playerChoice === 'paper' && computerChoice === 'rock'): return 'You won! Paper beats rock.';
-    case (playerChoice === 'paper' && computerChoice === 'scissors'): return 'You lost. Scissors beat paper.';
-    case (playerChoice === 'scissors' && computerChoice === 'paper'): return 'You won! scissors beat paper.';
-    case (playerChoice === 'scissors' && computerChoice === 'rock'): return 'You lost. Rock beats scissors.';
-    default: console.log('Something went wrong. Please reload the page and try again.');
+    case (playerChoice === 'rock' && computerChoice === 'scissors'):
+    case (playerChoice === 'paper' && computerChoice === 'rock'):
+    case (playerChoice === 'scissors' && computerChoice === 'paper'): 
+      return ('you won! ' + playerChoice + ' beats ' + computerChoice);
+    case (playerChoice === 'rock' && computerChoice === 'paper'):
+    case (playerChoice === 'paper' && computerChoice === 'scissors'):
+    case (playerChoice === 'scissors' && computerChoice === 'rock'): 
+      return ('you lost. ' + computerChoice + ' beats ' + playerChoice);
+    default: console.log('something went wrong. Please reload the page and try again.');
   }
 }
 
 //Create a function that plays 5 rounds of rock paper scissors
 function game() {
-  // Add a counter to keep track of player wins
+  // Add counters to keep track of player & computer wins
   let playerWins = 0;
-  // Add a counter to keep track of computer wins
   let computerWins = 0;
-
   // Play a best of 5
   for (let i = 0; i < 5; i++) {
-
-    // Play rock paper scissors
-    let gameResult = playRockPaperScissors();
+    const gameResult = playRockPaperScissors();
     console.log(gameResult);
-
     // Add to the winner's counter
-    if (gameResult.includes('You won!')) playerWins++;
-    if (gameResult.includes('You lost.')) computerWins++;
+    if (gameResult.includes('you won!')) playerWins++;
+    if (gameResult.includes('you lost.')) computerWins++;
     console.log(`You have ` + playerWins + ` wins. The computer has ` + computerWins + ' wins.');
-
     // End the game early if someone reaches three wins
     if (playerWins === 3) {
       console.log('You\'ve won the best of 5! Congratulations.')
