e4e5fa55e0bbffb62616c64b93f973c248154c5c
aru <110044109+im-aru@users.noreply.github.com>
2023-12-15 07:18:16 -0800

    Created best of 5 function & made messages to user more consistent
    Game can be played in a best of 5 & all messages go to the console so users aren't looking in multiple places.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,7 @@
-//odin-RPS Algorithm & Plan
-
 //Create a function that assigns the computer with either rock, paper, or scissors
-function getComputerChoice(){
-
+function getComputerChoice() {
   //Randomly generate a number
   let randomNumber = Math.random();
-  // console.log('The randomly generated number is ' + randomNumber);
 
   //Assign the computer's decision of rock, paper, or scissors based on the generated number.
   switch (true) {
@@ -19,32 +15,34 @@ function getComputerChoice(){
 function getPlayerChoice() {
   //Ask the user to select between rock, paper, or scissors. 
   const userChoice = prompt('do you choose rock, paper, or scissors?');
-  
+
   //Format to fit within the program.
   switch (userChoice?.toLowerCase()) {
     case ('rock'): return 'rock';
     case ('paper'): return 'paper';
-    case ('scissors'): return 'scissors';  
+    case ('scissors'): return 'scissors';
     //Create a catch to make sure the user doesn't select something other than rock, paper, or scissors
     default:
-      alert('invalid choice. please select rock, paper, or scissors.');
+      console.log('invalid choice. please select rock, paper, or scissors.');
       return getPlayerChoice();
   }
 }
 
 //Create a function to play rock paper scissors
-function playRockPaperScissors(playerChoice , computerChoice){
+function playRockPaperScissors(playerChoice, computerChoice) {
   //Get a new user choice
   playerChoice = getPlayerChoice();
   console.log('You have chosen ' + playerChoice);
+
   //Get a new computer choice
   computerChoice = getComputerChoice();
   console.log('The computer has chosen ' + computerChoice);
+
   //Compare answers to see who won
   switch (true) {
-    //If both answers are the same, re-run the program until one side wins.
+    //If both answers are the same, re-play the game until one side wins.
     case (playerChoice === computerChoice):
-      alert('The result is a tie! Please try again.');
+      console.log('The result is a tie! Please try again.');
       return playRockPaperScissors();
     case (playerChoice === 'rock' && computerChoice === 'scissors'): return 'You won! Rock beats scissors.';
     case (playerChoice === 'rock' && computerChoice === 'paper'): return 'You lost. Paper beats rock.';
@@ -52,17 +50,38 @@ function playRockPaperScissors(playerChoice , computerChoice){
     case (playerChoice === 'paper' && computerChoice === 'scissors'): return 'You lost. Scissors beat paper.';
     case (playerChoice === 'scissors' && computerChoice === 'paper'): return 'You won! scissors beat paper.';
     case (playerChoice === 'scissors' && computerChoice === 'rock'): return 'You lost. Rock beats scissors.';
-    default: alert('Something went wrong. Please reload the page and try again.');
+    default: console.log('Something went wrong. Please reload the page and try again.');
   }
 }
 
-//Create a variable to store the results of the game
-let gameResult;
+//Create a function that plays 5 rounds of rock paper scissors
+function game() {
+  // Add a counter to keep track of player wins
+  let playerWins = 0;
+  // Add a counter to keep track of computer wins
+  let computerWins = 0;
 
-//Create a variable to store the user's choice
-let playerChoice;
+  // Play a best of 5
+  for (let i = 0; i < 5; i++) {
 
-//Create a variable to store the computer's choice
-let computerChoice;
+    // Play rock paper scissors
+    let gameResult = playRockPaperScissors();
+    console.log(gameResult);
 
+    // Add to the winner's counter
+    if (gameResult.includes('You won!')) playerWins++;
+    if (gameResult.includes('You lost.')) computerWins++;
+    console.log(`You have ` + playerWins + ` wins. The computer has ` + computerWins + ' wins.');
 
+    // End the game early if someone reaches three wins
+    if (playerWins === 3) {
+      console.log('You\'ve won the best of 5! Congratulations.')
+      i = 5
+    } else if (computerWins === 3) {
+      console.log('You\'ve lost the best of 5. Good luck next time!')
+      i = 5
+    }
+  }
+}
+
+game();
